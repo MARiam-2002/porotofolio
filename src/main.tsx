b1819b69e16f8566889b85126bc28482b81fdae8
mariam khaled <khaldmrym769@gmail.com>
2025-09-01 02:02:30 +0300

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -23,7 +23,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
+            <BrowserRouter basename="/">
                 <App />
                 <Toaster
                     position="top-right"
