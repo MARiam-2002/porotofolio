7ad827e87d899c6a0f4e0992cb5b6dd9a04e7ef2
mariam khaled <khaldmrym769@gmail.com>
2025-09-01 02:09:04 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from './contexts/ThemeContext'
 import { LanguageProvider } from './contexts/LanguageContext'
 import Layout from './components/Layout'
-import PerformanceOptimizer from './components/PerformanceOptimizer'
+
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import ProjectDetails from './pages/ProjectDetails'
@@ -14,24 +14,22 @@ import NotFound from './pages/NotFound'
 
 function App() {
     return (
-        <PerformanceOptimizer>
-            <ThemeProvider>
-                <LanguageProvider>
-                    <Layout>
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/projects" element={<Projects />} />
-                            <Route path="/projects/:slug" element={<ProjectDetails />} />
-                            <Route path="/experience" element={<Experience />} />
-                            <Route path="/skills" element={<Skills />} />
-                            <Route path="/certifications" element={<Certifications />} />
-                            <Route path="/contact" element={<Contact />} />
-                            <Route path="*" element={<NotFound />} />
-                        </Routes>
-                    </Layout>
-                </LanguageProvider>
-            </ThemeProvider>
-        </PerformanceOptimizer>
+        <ThemeProvider>
+            <LanguageProvider>
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/projects/:slug" element={<ProjectDetails />} />
+                        <Route path="/experience" element={<Experience />} />
+                        <Route path="/skills" element={<Skills />} />
+                        <Route path="/certifications" element={<Certifications />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Layout>
+            </LanguageProvider>
+        </ThemeProvider>
     )
 }
 
